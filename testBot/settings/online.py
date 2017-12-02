97d4243af2872fbc9f7cd4e28032db2308acd71a
irikeish <allolro@gmail.com>
2017-12-03 04:35:33 +0530

    Update online.py

--- testBot/settings/online.py
+++ testBot/settings/online.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Bot/static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Bot/static'),
@@ -122,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = BASE_DIR+'/Bot/static/'
+STATIC_URL = BASE_DIR+'Bot/static/'
