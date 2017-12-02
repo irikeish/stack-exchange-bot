8a362e46b1b6928ff84881a8bbd9a79ec0f8eb9e
irikeish <allolro@gmail.com>
2017-12-03 05:26:32 +0530

    Update settings.py

--- testBot/settings.py
+++ testBot/settings.py
@@ -18,7 +18,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../static'),
 )
 
 # Quick-start development settings - unsuitable for production
