b17384fe2231790c661b32448fbd3e15a1148d82
irikeish <allolro@gmail.com>
2017-12-03 04:39:00 +0530

    Update local.py

--- testBot/settings/local.py
+++ testBot/settings/local.py
@@ -14,10 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = os.path.join(BASE_DIR, 'Bot/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/Bot/static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'Bot/static'),
+    os.path.join(BASE_DIR, '/Bot/static'),
 )
 
 # Quick-start development settings - unsuitable for production
@@ -122,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = BASE_DIR+'Bot/static/'
+STATIC_URL = BASE_DIR+'/Bot/static/'
